58bbdda79d123db81b3a74d0a488fdc60548b83a
AKHIL PA <akhilmanojpa0@gmail.com>
2021-09-23 15:27:17 +0530

    Location Buttons activated using javascript

---
+++ script.js
@@ -0,0 +1,25 @@
+function kadalpalam(){
+    document.getElementById("kadal-palam").onclick= function()  {
+        location.href = "https://www.google.com/maps/place/Thalassery+kadalpalam/@11.74549,75.4870692,17z/data=!3m1!4b1!4m5!3m4!1s0x3ba427cbe3d2849f:0xf844c04c946751d!8m2!3d11.7455022!4d75.4892645";
+}
+}
+
+function ThalasseryFort(){
+    document.getElementById("tlyfort").onclick= function(){
+        location.href = "https://www.google.com/maps/place/Thalassery+Fort/@11.7481105,75.4843994,17z/data=!3m1!4b1!4m5!3m4!1s0x3ba42656b0d90a13:0x1b409cfe11125095!8m2!3d11.7481105!4d75.4865881"
+    }
+}
+
+
+function HotelVictoria(){
+    document.getElementById("victoria").onclick= function(){
+        location.href = "https://www.google.com/maps/place/Hotel+Victoria/@11.7572114,75.4768658,17z/data=!3m1!4b1!4m8!3m7!1s0x3ba426f66fb87e67:0x1492e054ff6721f5!5m2!4m1!1i2!8m2!3d11.7572114!4d75.4790545"
+    }
+}
+
+
+function ParisResidency(){
+    document.getElementById("paris").onclick= function(){
+        location.href = "https://www.google.com/maps/place/Hotel+Paris+Presidency/@11.7476942,75.4878722,17z/data=!3m1!4b1!4m8!3m7!1s0x3ba426518006a9c9:0x1079b860e3e2dab8!5m2!4m1!1i2!8m2!3d11.7476871!4d75.4900602"
+    }
+}
